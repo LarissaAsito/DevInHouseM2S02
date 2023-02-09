f6115c96888e9deeb0bbdfe98f971157b82a4007
Larissa <larissa.asito@gmail.com>
2023-02-09 20:07:19 -0300

    Adicionando exercicio 1

---
+++ Exercicios/src/Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+
+        System.out.println("Primeiro elemento " + arr[0]);
+        System.out.println("Último elemento " + arr[7]);
+        System.out.println("Terceiro elemento " + arr[2]);
+    }
+}
