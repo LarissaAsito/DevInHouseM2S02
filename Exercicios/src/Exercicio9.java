dbd144be780711c262a353a7ecdcf2f00747482c
Larissa <larissa.asito@gmail.com>
2023-02-09 22:44:10 -0300

    Adicionando exercicio 9

---
+++ Exercicios/src/Exercicio9.java
@@ -0,0 +1,12 @@
+public class Exercicio9 {
+    public static void main(String[] args) {
+        int[] sequencia = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        for(int num: sequencia){
+            if(num%3==0){
+                System.out.print("PIN ");
+            }else{
+                System.out.printf("%d ", num);
+            }
+        }
+    }
+}
